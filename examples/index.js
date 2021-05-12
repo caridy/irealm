c13cdac5d30b373bc1076fb9d4bfea6cc1f9a419
Leo Balter <leonardo.balter@gmail.com>
2021-05-12 12:28:13 -0700

    Expand examples to compare with iframes

--- examples/index.js
+++ examples/index.js
@@ -1,10 +1,6 @@
 import localInit  from '../lib/init.js';
 
-// const iframe = document.body.querySelector('iframe');
-// const evaluate = iframe.contentWindow.eval;
-
-const realm = new Realm();
-const evaluate = realm.evaluate.bind(realm);
+const { evaluate } = globalThis;
 
 const localInitSourceText = `(${localInit.toString()})`;
 
